98cf0a268daef450d98650a45be434f28669f971
luiz.marins <luiz.marins@liferay.com>
2018-10-10 10:10:41 -0300

    Create sample project

---
+++ modules/test/integration-tests/integration-test-example/src/testIntegration/java/com/liferay/integration/test/MyIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.liferay.integration.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.Sync;
+import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Luiz Marins
+ */
+@RunWith(Arquillian.class)
+@Sync
+public class MyIntegrationTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			SynchronousDestinationTestRule.INSTANCE);
+
+	@Test
+	public void test() throws Exception {
+		System.out.println("test");
+	}
+
+}
